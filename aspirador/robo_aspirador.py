0df6adac8ec17b2b51a66c62f98b3df2eff42da0
Jonas Pelegrina <jonasbonfapelegrina@gmail.com>
2024-08-26 12:15:26 +0000

    Aspirador com 4 quartos

--- aspirador/robo_aspirador.py
+++ aspirador/robo_aspirador.py
@@ -3,29 +3,45 @@ from aigyminsper.search.Graph import State
 
 class RoboAspirador(State):
 
-    def __init__(self, op,posicao_robo, situacao_esq, situacao_dir):
+    def __init__(self, op,posicao_robo, situacao_esq, situacao_dir,situacao_cima,situacao_baixo):
         # You must use this name for the operator!
         self.operator = op
         self.posicao_robo = posicao_robo
         self.situacao_esq = situacao_esq
         self.situacao_dir = situacao_dir
+        self.situacao_cima = situacao_cima
+        self.situacao_baixo = situacao_baixo
+
     
     def successors(self):
         successors = []
         # Unica ação é mover o robo para a esquerda - Resto igual portanto pega do self
-        # IR PARA A ESQUERDA - CONSEQUÊNCIA
-        successors.append(RoboAspirador('Ir para a esquerda','ESQ',self.situacao_esq,self.situacao_dir))
 
-        # IR PARA A DIREITRA
-        successors.append(RoboAspirador('Ir para a direita','DIR',self.situacao_esq,self.situacao_dir))
+        # IR PARA A ESQUERDA - CONSEQUÊNCIA
+        successors.append(RoboAspirador('Ir para a esquerda','1',self.situacao_esq,self.situacao_dir,self.situacao_cima,self.situacao_baixo))
+
+        # IR PARA A DIREITA
+        successors.append(RoboAspirador('Ir para a direita','2',self.situacao_esq,self.situacao_dir,self.situacao_cima,self.situacao_baixo))
+
+        # IR PARA A CIMA
+        successors.append(RoboAspirador('Ir para a direita','3',self.situacao_esq,self.situacao_dir,self.situacao_cima,self.situacao_baixo))
+        
+        # IR PARA A BAIXO
+        successors.append(RoboAspirador('Ir para a direita','4',self.situacao_esq,self.situacao_dir,self.situacao_cima,self.situacao_baixo))
 
         # LIMPAR
-        # LIMPAR A ESQUERDA
-        if self.posicao_robo == 'ESQ':
-            successors.append(RoboAspirador('Limpar',self.posicao_robo,'LIMPO',self.situacao_dir))
+        # LIMPAR A 1
+        if self.posicao_robo == '1':
+            successors.append(RoboAspirador('Limpar',self.posicao_robo,'LIMPO',self.situacao_dir,self.situacao_cima,self.situacao_baixo))
         # LIMPAR A DIREITA
-        if self.posicao_robo == 'DIR':
-            successors.append(RoboAspirador('Limpar',self.posicao_robo,self.situacao_esq,'LIMPO'))
+        if self.posicao_robo == '2':
+            successors.append(RoboAspirador('Limpar',self.posicao_robo,self.situacao_esq,'LIMPO',self.situacao_cima,self.situacao_baixo))
+
+        if self.posicao_robo == '3':
+            successors.append(RoboAspirador('Limpar',self.posicao_robo,self.situacao_esq,self.situacao_dir,'LIMPO',self.situacao_baixo))
+
+        if self.posicao_robo == '4':
+            successors.append(RoboAspirador('Limpar',self.posicao_robo,self.situacao_esq,self.situacao_dir,self.situacao_cima,'LIMPO'))
 
         return successors
     
